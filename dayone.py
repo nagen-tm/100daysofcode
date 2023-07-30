3aacadaeb78e144606c14056f25a5a8814a24087
Therese Nagengast <nagengtm+github@gmail.com>
2023-07-30 19:36:51 -0400

    update day one

---
+++ dayone.py
@@ -0,0 +1,7 @@
+# Create simple variable solution for inputs
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+# string literal allows variables in string without concatenation 
+print(f"Your band name could be {city} {pet}")
