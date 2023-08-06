9c347aec5a101f505989ea389fe334334ad27d33
Therese Nagengast <nagengtm+github@gmail.com>
2023-08-05 22:09:57 -0400

    update day 6 and 7, added hangman

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Follows 100 days of code [Udemy Course](https://www.udemy.com/course/100-days-of
 
 Beginner folder has days 1-14, with the final project of the day. 
 
+Functions and while loops [challenge](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
+
 ## Python Environment
 I set up [miniforge](https://github.com/conda-forge/miniforge) for python
 
---
+++ hangman/hangman.py
@@ -0,0 +1,47 @@
+import random
+from os import system
+from hangman_art import logo, stages
+
+# pick random word, set variables
+#TO DO: create a word list to import
+word_list = ["ardvark", "baboon", "camel"]
+word_selection = random.choice(word_list)
+display = [] 
+lives = 6
+end_of_game = False
+
+# check letter in word
+for letter in word_selection:
+    display.append("_")
+
+print(logo)
+
+# get input 
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    # clear the screen 
+    system('clear')
+    # if letter was already guessed, tell user
+    if guess in display:
+        print(stages[lives])
+        print(f"You already guessed {guess}.")
+    else:
+        # check letter for each position
+        for position in range(len(word_selection)):
+            letter = word_selection[position]
+            if letter == guess:
+                display[position] = letter
+        # lose life if guess is wrong
+        if guess not in word_selection:
+            print(f"{guess} is not in the word. You lose a life.")
+            lives -= 1
+            # end game at 0 lives
+            if lives == 0: 
+                end_of_game = True
+                print("You lose.")
+        # end game if guessed word
+        if "_" not in display:
+            end_of_game = True
+            print("You win.")
+        print(stages[lives])
+        print(f"{' '.join(display)}")
